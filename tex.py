44f1a63deb598694218e676d47c48d96947bcf6a
Altashfirs <91253069+Altashfirs@users.noreply.github.com>
2021-09-28 23:58:22 +0700

    Update tex.py

--- tex.py
+++ tex.py
@@ -41,12 +41,11 @@ def absen():
     time.sleep(0.5)
     driver.find_element_by_css_selector("#mG61Hd > div.freebirdFormviewerViewFormCard.exportFormCard > div > div.freebirdFormviewerViewNavigationNavControls > div > div.freebirdFormviewerViewNavigationLeftButtons > div.appsMaterialWizButtonEl.appsMaterialWizButtonPaperbuttonEl.appsMaterialWizButtonPaperbuttonFilled.freebirdFormviewerViewNavigationSubmitButton.freebirdThemedFilledButtonM2 > span").click()
 
-absen()
-# while True:
-# #                     v----------------- on minute 0, so every full hour
-# #                       v--------------- on hours 9 till 16
-# #                            v v-------- every day in month and every month
-# #                                v------ on weekdays monday till friday
-#     if pycron.is_now('30 7 * * mon-fri'):
-#         absen()
-#     time.sleep(60)
+while True:
+#                     v----------------- on minute 0, so every full hour
+#                       v--------------- on hours 9 till 16
+#                            v v-------- every day in month and every month
+#                                v------ on weekdays monday till friday
+    if pycron.is_now('30 7 * * 1-5'):
+        absen()
+    time.sleep(60)
